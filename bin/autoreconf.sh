e6efe6cf91d236ccd78c3948de855b419b489994
Alessio Deiana <adeiana@gmail.com>
2014-09-11 14:47:31 +0200

    gettext automake macro in autoreconf.sh

--- bin/autoreconf.sh
+++ bin/autoreconf.sh
@@ -2,8 +2,8 @@
 
 CFG_INVENIO_PREFIX=${CFG_INVENIO_PREFIX:=/opt/invenio}
 
-aclocal && automake -a && autoconf && ./configure \
+aclocal -I /usr/local/Cellar/gettext/0.19.2/share/aclocal && automake -a && autoconf && ./configure \
    --prefix="$CFG_INVENIO_PREFIX"  \
-   --with-libintl-prefix=/opt/local \
-   #--with-openoffice-python=/Applications/LibreOffice.app/Contents/MacOS/python
+   # --with-libintl-prefix=/opt/macports \
+   # --with-openoffice-python=/Applications/LibreOffice.app/Contents/MacOS/python
    #--with-python=/opt/local/bin/python
