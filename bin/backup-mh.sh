1bfae056e8e994fcb8764102ce7b538ac546e318
Alessio Deiana <adeiana@gmail.com>
2015-09-23 15:46:59 +0200

    Updates mh backup script

--- bin/backup-mh.sh
+++ bin/backup-mh.sh
@@ -1,3 +1,3 @@
-ssh root@nas "rsync -avP root@mangahelpers.com:/var/backups /mnt/array/mh-backups/"
-ssh root@nas "rsync -avP root@mangahelpers.com:/var/mangahelpers /mnt/array/mh-backups/"
+#ssh root@nas "rsync -avP osso@mangahelpers.com:/var/backups /mnt/array/mh-backups/"
+ssh root@nas "rsync -avP osso@mangahelpers.com:/var/mangahelpers /mnt/array/mh-backups/"
 
