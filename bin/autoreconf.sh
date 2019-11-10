954cc6c21d662e39c5b5053f0e3046793901447b
Alessio Deiana <adeiana@gmail.com>
2019-11-10 09:33:04 -0600

    autoreconf.sh: linux updates

--- bin/autoreconf.sh
+++ bin/autoreconf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-aclocal -I /usr/local/Cellar/gettext/0.19.2/share/aclocal && \
-    automake -a && \
-    autoconf && \
-    ./configure
-   # --with-openoffice-python=/Applications/LibreOffice.app/Contents/MacOS/python
-   #--with-python=/opt/local/bin/python
+set -e
+
+aclocal
+automake -a
+autoconf
+./configure
