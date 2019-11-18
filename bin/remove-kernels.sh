fab29284da84113d0c65685c09b7e8646ba727f3
Alessio Deiana <adeiana@gmail.com>
2019-11-18 17:12:41 -0600

    bin: remove remove_kernels.sh

--- bin/remove-kernels.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-apt list 'linux-image*' 2>/dev/null | grep 'installed' | cut -f1 -d'/' | grep $1 | xargs sudo apt remove -y
-apt list 'linux-image*' 2>/dev/null | grep 'residual-config' | cut -f1 -d'/' | xargs sudo apt remove -y --purge
